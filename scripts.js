edc63118b633291610cba4e4128c717066fa32bf
Guido Bacce <gbacce@MacBook-Pro.local>
2017-05-10 15:56:31 -0400

    init commit

---
+++ scripts.js
@@ -0,0 +1,42 @@
+function Timer(id, endtime) {
+	this.endtime = endtime;
+	this.id = id;
+	var clock = document.getElementById(id);
+	this.weeksSpan = clock.querySelector('.weeks');  /* Alternatively, you could use document.getElementsByClassName('.weeks');*/
+	this.daysSpan = clock.querySelector('.days');  /*If you were looking for an ID, you would write .querySelector('#days')*/
+	this.hoursSpan = clock.querySelector('.hours');
+	this.minutesSpan = clock.querySelector('.minutes');
+	this.secondsSpan = clock.querySelector('.seconds');
+}
+
+Timer.prototype.getTimeRemaining = function() {
+	t = Date.parse(this.endtime) - Date.parse(new Date());
+	this.seconds = Math.floor((t / 1000) % 60);
+	this.minutes = Math.floor((t / 1000 / 60) % 60);
+	this.hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	this.days = Math.floor((t / (1000 * 60 * 60 * 24)) % 7);
+	this.weeks = Math.floor(t / (1000 * 60 * 60 * 24 * 7));
+}
+
+Timer.prototype.updateTimer = function() {
+	this.getTimeRemaining();
+	this.weeksSpan.innerHTML = this.weeks;
+	this.daysSpan.innerHTML = this.days;
+	this.hoursSpan.innerHTML = this.hours;
+	this.minutesSpan.innerHTML = this.minutes;
+	this.secondsSpan.innerHTML = this.seconds;
+}
+
+// var endDate = new Date();
+var endDate = new Date(Date.parse('August 9, 2017')); /*Converts date into a UNIX timestamp in milliseconds*/
+
+//console.log(endDate);
+var graduationTimer = new Timer('timer-div', endDate);
+setInterval(
+	function() {
+		graduationTimer.updateTimer();
+	}, 1000
+)
+
+
+// august 9 4pm
